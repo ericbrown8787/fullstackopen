04daa81b7415691be3455219ffad50b873024f9f
Eric Brown <eric.brown8787@gmail.com>
2024-08-09 10:23:09 -0400

    Complete exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,20 +15,40 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  const calculateAverage = (good, neutral, bad) => {
+    return (good - bad) / (good + neutral + bad);
+  };
+
+  const calculatePositive = (good, neutral, bad) => {
+    return (good / (good + bad + neutral)) * 100;
+  };
 
   const handleGoodClick = () => {
     const updatedGood = good + 1;
     setGood(updatedGood);
+    setAll(updatedGood + neutral + bad);
+    setAverage(calculateAverage(updatedGood, neutral, bad));
+    setPositive(calculatePositive(updatedGood, neutral, bad));
   };
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1;
     setNeutral(updatedNeutral);
+    setAll(good + updatedNeutral + bad);
+    setAverage(calculateAverage(good, updatedNeutral, bad));
+    setPositive(calculatePositive(good, updatedNeutral, bad));
   };
 
   const handleBadClick = () => {
     const updatedBad = bad + 1;
     setBad(updatedBad);
+    setAll(good + neutral + updatedBad);
+    setAverage(calculateAverage(good, neutral, updatedBad));
+    setPositive(calculatePositive(good, neutral, updatedBad));
   };
   return (
     <div>
@@ -40,6 +60,9 @@ const App = () => {
       <Stat statName="good" statValue={good} />
       <Stat statName="neutral" statValue={neutral} />
       <Stat statName="bad" statValue={bad} />
+      <Stat statName="all" statValue={all} />
+      <Stat statName="average" statValue={average} />
+      <Stat statName="positive" statValue={`${positive} %`} />
     </div>
   );
 };
