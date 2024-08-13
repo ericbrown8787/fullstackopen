e446ee7dae745d545c0fc4c0b7a73d5573d33ed9
Eric Brown <eric.brown8787@gmail.com>
2024-08-13 15:49:37 -0400

    Complete exercise 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,5 +1,31 @@
-import Header from "./Header";
-import Content from "./Content";
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+      <Total parts={parts} />
+    </>
+  );
+};
+const Header = ({ text }) => {
+  return <h2>{text}</h2>;
+};
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+const Total = ({ parts }) => {
+  const exerciseCounts = parts.map((part) => part.exercises);
+  const total = exerciseCounts.reduce(
+    (accumulator, current) => accumulator + current
+  );
+  return <strong>total of {total} exercises</strong>;
+};
+
 const Course = ({ course }) => (
   <div>
     <Header text={course.name} />
--- part2/courseinfo/src/components/Total.jsx
+++
@@ -1,9 +0,0 @@
-const Total = ({ parts }) => {
-  const exerciseCounts = parts.map((part) => part.exercises);
-  const total = exerciseCounts.reduce(
-    (accumulator, current) => accumulator + current
-  );
-  return <strong>total of {total} exercises</strong>;
-};
-
-export default Total;
